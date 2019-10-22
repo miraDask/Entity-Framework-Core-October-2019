103fa0088024c0d0b0d00ff87affc8e7703b40fd
miraDask <mira.daskalov@gmail.com>
2019-10-22 15:34:09 +0100

    p01

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/01. Initial Setup/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace _01._Initial_Setup
+{
+    using System;
+    using System.Data.SqlClient;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+
+            string serverName = Console.ReadLine();
+
+            ConnectionStringBuilder csBuilder = new ConnectionStringBuilder(serverName);
+            string connectionString = csBuilder.GetConnectionString(Constants.MasterDB);
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                try
+                {
+                    string queryText = String.Format(Constants.CreateDBText, Constants.ClientDB);
+                    SqlCommand createDB = new SqlCommand(queryText, connection);
+                    createDB.ExecuteNonQuery();
+                    Console.WriteLine(Constants.DatabaseCreatedMessage);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+           
+            connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                
+                try
+                {
+                    SqlCommand createTables = new SqlCommand(Constants.CreateQueryText, connection);
+                    createTables.ExecuteNonQuery();
+                    Console.WriteLine(Constants.TablesCreatedMessage);
+
+                    SqlCommand insertCommand = new SqlCommand(Constants.InsertQueryText, connection);
+                    int affectedRows = insertCommand.ExecuteNonQuery();
+                    Console.WriteLine(Constants.AffectedRowsMessage, affectedRows);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
