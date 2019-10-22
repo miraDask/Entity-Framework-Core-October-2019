ff5316a56cc2912e22ad52efded54d110e4321b6
miraDask <mira.daskalov@gmail.com>
2019-10-22 17:25:13 +0100

    p02

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/02. VillainNames/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._VillainNames
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+
+        public const string QueryText = @" SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                             FROM Villains AS v 
+                                             JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                         GROUP BY v.Id, v.Name 
+                                           HAVING COUNT(mv.VillainId) > 3 
+                                         ORDER BY COUNT(mv.VillainId)";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/02. VillainNames/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _02._VillainNames
+{
+    
+    using System;
+    using System.Data.SqlClient;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                try
+                {
+                    var command = new SqlCommand(Constants.QueryText, connection);
+                    var reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]} - {reader[1]}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                   
+                }
+            }
+        }
+    }
+}
