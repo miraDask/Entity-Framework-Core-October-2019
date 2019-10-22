459321267b44cc80e662850ec5339cb31393734a
miraDask <mira.daskalov@gmail.com>
2019-10-22 19:36:59 +0100

    p04

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/04. AddMinion/Constants.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._AddMinion
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+
+        public const string InputDataInvitationText = "Insert information about minion and villian and press Enter.";
+
+        public const string VillianIdQuery = @"SELECT Id FROM Villains WHERE Name = '{0}'";
+
+        public const string MinionIdQuery = @"SELECT Id FROM Minions WHERE Name = '{0}'";
+
+        public const string InsertIntoMappingTable = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES ({0}, {1})";
+
+        public const string InsertIntoVillians = @"INSERT INTO Villains (Name, EvilnessFactorId)  VALUES ('{0}', 4)";
+
+        public const string InsertIntoMinions = @"INSERT INTO Minions (Name, Age, TownId) VALUES ('{0}', {1}, {2})";
+
+        public const string InsertIntoTowns = @"INSERT INTO Towns (Name) VALUES ('{0}')";
+
+        public const string TownCheck = @"SELECT Id FROM Towns WHERE Name = '{0}'";
+
+        public const string TownIsAddedMessage = @"Town {0} was added to the database.";
+
+        public const string VillianIsAddedMessage = @"Villiant {0} was added to the database.";
+
+        public const string MinionIsAddedMessage = @"Minion {0} was added to the database.";
+
+        public const string MinionIsAddedToVillianMessage = @"Successfully added {0} to be minion of {1}.";
+
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/04. AddMinion/StartUp.cs
@@ -0,0 +1,130 @@
+﻿namespace _04._AddMinion
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            Console.WriteLine(Constants.InputDataInvitationText);
+            var minionName = string.Empty;
+            var minionAge = 0;
+            var minionTown = string.Empty;
+            var villianName = string.Empty;
+
+            for (int i = 0; i < 2; i++)
+            {
+                var data = Console.ReadLine().Split();
+                
+                if (i == 0)
+                {
+                    minionName = data[1];
+                    minionAge = int.Parse(data[2]);
+                    minionTown = data[3];
+                }
+                else
+                {
+                    villianName = data[1];
+                }
+            }
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlTransaction transaction = null;
+
+            using (connection)
+            {
+                var minionIsAdded = false;
+                var villiantIsAdded = false;
+                var townIsAdded = false;
+
+                try
+                {
+                    transaction = connection.BeginTransaction();
+
+                    var command = new SqlCommand(string.Format(Constants.TownCheck, minionTown), connection);
+                    command.Transaction = transaction;
+                    var townId = command.ExecuteScalar();
+
+                    if(townId == null)
+                    {
+                        command.CommandText = string.Format(Constants.InsertIntoTowns, minionTown);
+                        command.ExecuteNonQuery();
+                        townIsAdded = true;
+                        
+                        command.CommandText = string.Format(Constants.TownCheck, minionTown);
+                        townId = command.ExecuteScalar();
+                    }
+
+                    command.CommandText = string.Format(Constants.VillianIdQuery, villianName);
+                    var vilianId = command.ExecuteScalar();
+
+                    if(vilianId == null)
+                    {
+                        command.CommandText = string.Format(Constants.InsertIntoVillians, villianName);
+                        command.ExecuteNonQuery();
+                        villiantIsAdded = true;
+
+                        command.CommandText = string.Format(Constants.VillianIdQuery, villianName);
+                        vilianId = command.ExecuteScalar();
+                    }
+
+                    command.CommandText = string.Format(Constants.MinionIdQuery, minionName);
+                    var minionId = command.ExecuteScalar();
+
+                    if (minionId == null)
+                    {
+                        command.CommandText = string.Format(Constants.InsertIntoMinions, minionName, minionAge, townId);
+                        command.ExecuteNonQuery();
+                        minionIsAdded = true;
+                        
+                        command.CommandText = string.Format(Constants.MinionIdQuery, minionName);
+                        minionId = command.ExecuteScalar();
+                    }
+
+                    command.CommandText = string.Format(Constants.InsertIntoMappingTable, minionId, vilianId);
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    if(townIsAdded)
+                    {
+                        Console.WriteLine(string.Format(Constants.TownIsAddedMessage, minionTown));
+                    }
+
+                    if (villiantIsAdded)
+                    {
+                        Console.WriteLine(string.Format(Constants.VillianIsAddedMessage, villianName));
+                    }
+
+                    if (minionIsAdded)
+                    {
+                        Console.WriteLine(string.Format(Constants.MinionIsAddedMessage, minionName));
+                    }
+                    
+                    Console.WriteLine(string.Format(Constants.MinionIsAddedToVillianMessage, minionName, villianName));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                   
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
+           
