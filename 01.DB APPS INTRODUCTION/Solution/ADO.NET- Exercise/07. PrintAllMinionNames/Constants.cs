a053e0b0de2601c27f51ea11f9d74502fb85fcc9
miraDask <mira.daskalov@gmail.com>
2019-10-23 22:56:33 +0100

    adding p07

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/07. PrintAllMinionNames/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace _07._PrintAllMinionNames
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+
+        public const string GetMinionsNames = @"SELECT Name FROM Minions";
+
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/07. PrintAllMinionNames/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _07._PrintAllMinionNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                try
+                {
+
+                    var command = new SqlCommand(Constants.GetMinionsNames, connection);
+                    var reader = command.ExecuteReader();
+                    var minionsList = new List<string>();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            minionsList.Add(reader[0].ToString());
+                        }
+                    }
+
+                    var halfMinionsCount = minionsList.Count / 2;
+
+                    for (int i = 0; i < halfMinionsCount; i++)
+                    {
+                        Console.WriteLine(minionsList[i]);
+
+                        Console.WriteLine(minionsList[minionsList.Count - 1 - i]);
+                    }
+
+                    if (minionsList.Count % 2 == 1)
+                    {
+                        Console.WriteLine(minionsList[halfMinionsCount]);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
