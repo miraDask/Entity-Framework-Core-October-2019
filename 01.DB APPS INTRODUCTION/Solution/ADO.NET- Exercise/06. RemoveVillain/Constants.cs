bc7d056c76ad79ba2f3775dd2aaa089dbe83c240
miraDask <mira.daskalov@gmail.com>
2019-10-23 22:17:14 +0100

    adding p06

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/06. RemoveVillain/Constants.cs
@@ -0,0 +1,25 @@
+﻿namespace _06._RemoveVillain
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+
+        public const string InputVilliantId = "Insert villiant Id and press Enter.";
+
+        public const string GetVilliantNameQuery = @"SELECT Name FROM Villains WHERE Id = {0}";
+
+        public const string DeleteFromMappingtable = @"DELETE FROM MinionsVillains 
+                                                        WHERE VillainId = {0}";
+
+        public const string DeleteFromVillains = @"DELETE FROM Villains
+                                                    WHERE Id = {0}";
+
+        public const string SuccessMessage = @"{0} was deleted.
+{1} minions were released.";
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/06. RemoveVillain/StartUp.cs
@@ -0,0 +1,67 @@
+﻿
+namespace _06._RemoveVillain
+{
+    
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            Console.WriteLine(Constants.InputVilliantId);
+            var villianId = Console.ReadLine();
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlTransaction transaction = null;
+
+            using (connection)
+            {
+                
+                try
+                {
+                    transaction = connection.BeginTransaction();
+                    
+                    var command = new SqlCommand(string.Format(Constants.GetVilliantNameQuery, villianId), connection);
+                    command.Transaction = transaction;
+                    var villianName = command.ExecuteScalar();
+
+                    if (villianName == null)
+                    {
+                        throw new NullReferenceException("No such villain was found.");
+                    }
+
+                    command.CommandText = string.Format(Constants.DeleteFromMappingtable, villianId);
+                    var rowsAffected = command.ExecuteNonQuery();
+
+                    command.CommandText = string.Format(Constants.DeleteFromVillains, villianId);
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    Console.WriteLine(Constants.SuccessMessage, villianName, rowsAffected);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
