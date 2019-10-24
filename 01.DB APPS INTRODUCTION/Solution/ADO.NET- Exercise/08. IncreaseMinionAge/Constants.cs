d032c151e53c83a8a23a168cb5f6300f8f55d695
miraDask <mira.daskalov@gmail.com>
2019-10-24 23:47:15 +0100

    adding p08

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/08. IncreaseMinionAge/Constants.cs
@@ -0,0 +1,22 @@
+﻿namespace _08._IncreaseMinionAge
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+
+        public const string InputMinionsId = "Insert Ids of minions separated by space.";
+
+        public const string UpdateMinionName = @"UPDATE Minions
+                                                   SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                                 WHERE Id = {0}";
+        
+        public const string GetMinionData = @"SELECT Name, Age FROM Minions";
+
+        public const string PrintPattern = "{0} {1}";
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/08. IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _08._IncreaseMinionAge
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            Console.WriteLine(Constants.InputMinionsId);
+            var ids = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                try
+                {
+                    SqlCommand command = null;
+
+                    for (int i = 0; i < ids.Length; i++)
+                    {
+                        command = new SqlCommand(string.Format(Constants.UpdateMinionName, ids[i]), connection);
+                        command.ExecuteNonQuery();
+                    }
+
+                    command.CommandText = Constants.GetMinionData;
+                    var reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(string.Format(Constants.PrintPattern, reader[0], reader[1]));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
