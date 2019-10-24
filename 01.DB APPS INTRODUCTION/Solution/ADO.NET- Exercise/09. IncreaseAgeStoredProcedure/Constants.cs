dc5831b30dc1204fcfcdf153affacab3f8604339
miraDask <mira.daskalov@gmail.com>
2019-10-24 23:47:49 +0100

    adding p09

---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/09. IncreaseAgeStoredProcedure/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace _09._IncreaseAgeStoredProcedure
+{
+    using System;
+
+    public static class Constants
+    {
+
+        public const string ClientDB = "MinionsDB";
+
+        public const string InputInvitationText = "Insert your server name please and press Enter!";
+
+        public const string InputMinionId = "Insert Id of minion and press Enter.";
+
+        public const string IncreaseMinionAge = @"EXEC dbo.usp_GetOlder {0}";
+
+        public const string GetData = @"SELECT Name, Age FROM Minions WHERE Id = {0}";
+
+        public const string OutputPattern = "{0} – {1} years old";
+        
+    }
+}
---
+++ 01.DB APPS INTRODUCTION/Solution/ADO.NET- Exercise/09. IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace _09._IncreaseAgeStoredProcedure
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Constants.InputInvitationText);
+            var serverName = Console.ReadLine();
+
+            Console.WriteLine(Constants.InputMinionId);
+            var id = Console.ReadLine();
+
+            var csBuilder = new ConnectionStringBuilder(serverName);
+            var connectionString = csBuilder.GetConnectionString(Constants.ClientDB);
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)
+            {
+                try
+                {
+                    var command = new SqlCommand(string.Format(Constants.IncreaseMinionAge, id), connection);
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = string.Format(Constants.GetData, id);
+                    var reader = command.ExecuteReader();
+
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(string.Format(Constants.OutputPattern, reader[0], reader[1]));
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
