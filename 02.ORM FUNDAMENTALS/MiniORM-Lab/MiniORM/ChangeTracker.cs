d28cb110ac1aed7069d452991f01663aa1650433
miraDask <mira.daskalov@gmail.com>
2019-10-26 20:41:03 +0100

    initial

---
+++ 02.ORM FUNDAMENTALS/MiniORM-Lab/MiniORM/ChangeTracker.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MiniORM
+{
+    using System.Collections.Generic;
+	
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+    }
+}
