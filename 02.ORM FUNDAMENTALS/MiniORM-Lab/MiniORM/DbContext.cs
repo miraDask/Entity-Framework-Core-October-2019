39f783756232043e34beb0df3b25e6cff8c280e3
miraDask <mira.daskalov@gmail.com>
2019-10-27 14:58:24 +0000

    partial DbContext

--- 02.ORM FUNDAMENTALS/MiniORM-Lab/MiniORM/DbContext.cs
+++ 02.ORM FUNDAMENTALS/MiniORM-Lab/MiniORM/DbContext.cs
@@ -1,8 +1,141 @@
-﻿namespace MiniORM
-{
-    internal class DbContext
-    {
+﻿
 
+namespace MiniORM
+{
+    using System;
+    using System.Reflection;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Data.SqlClient;
+    using System.Collections;
+
+    public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        protected DbContext(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+
+            this.dbSetProperties = DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {
+                this.InitializeDbSets();
+            }
+
+            this.MapAllRelations();
+        }
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+
+        public void SaveChanges()
+        {
+            object[] dbSets = this.dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                object[] invalidEntities = dbSet
+                    .Where(entity => !IsObjectValid(entity))
+                    .ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException($"{invalidEntities.Length} Invalid Entities found in {dbSet.GetType().Name}!");
+                }
+
+            }
+
+            using (new ConnectionManager(connection))
+            {
+                using (SqlTransaction transaction = this.connection.StartTransaction())
+                {
+                    foreach (IEnumerable dbSet in dbSets)
+                    {
+                        Type dbSetType = dbSet
+                            .GetType()
+                            .GetGenericArguments()
+                            .First();
+
+                        MethodInfo persistMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic)
+                            .MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+                        catch (TargetInvocationException tie)
+                        {
+                            throw tie.InnerException;
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                        catch (SqlException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+            }
+        }
+
+        private void Persist<TEntity>(DbSet<TEntity> dbSet)
+            where TEntity : class, new()
+        {
+            string tableName = this.GetTableName(typeof(TEntity));
+
+            string[] columns = this.connection
+                .FetchColumnNames(tableName)
+                .ToArray();
+            
+
+        }
+
+        private string GetTableName(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsObjectValid(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
