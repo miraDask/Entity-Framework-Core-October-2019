40a2eb03008a3331aeff68910222d6c077f34149
miraDask <mira.daskalov@gmail.com>
2019-10-27 18:46:22 +0000

    finished MapCollection method

--- 02.ORM FUNDAMENTALS/MiniORM-Lab/MiniORM/DbContext.cs
+++ 02.ORM FUNDAMENTALS/MiniORM-Lab/MiniORM/DbContext.cs
@@ -8,6 +8,8 @@ namespace MiniORM
     using System.Linq;
     using System.Data.SqlClient;
     using System.Collections;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public abstract class DbContext
     {
@@ -108,8 +110,146 @@ namespace MiniORM
             string[] columns = this.connection
                 .FetchColumnNames(tableName)
                 .ToArray();
-            
 
+            if (dbSet.ChangeTracker.Added.Any())
+            {
+                this.connection.InsertEntities(dbSet.ChangeTracker.Added, tableName, columns);
+            }
+
+            TEntity[] modifiedEntities = dbSet
+                .ChangeTracker
+                .GetModifiedEntities(dbSet)
+                .ToArray();
+
+            if (modifiedEntities.Any())
+            {
+                this.connection.UpdateEntities(modifiedEntities, tableName, columns);
+            }
+
+            if (dbSet.ChangeTracker.Removed.Any())
+            {
+                this.connection.DeleteEntities(dbSet.ChangeTracker.Removed, tableName, columns);
+            }
+        }
+
+        private void InitializeDbSets()
+        {
+            foreach (var dbSet in this.dbSetProperties)
+            {
+                Type dbSetType = dbSet.Key;
+                PropertyInfo dbSetProperty = dbSet.Value;
+
+                MethodInfo populateDbSetGeneric = typeof(DbContext)
+                    .GetMethod("PopulateDbSet", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(dbSetType);
+
+                populateDbSetGeneric.Invoke(this, new object[] { dbSetProperty });
+            }
+        }
+
+        private void PopulateDbSet<TEntity>(PropertyInfo dbSet)
+            where TEntity : class, new()
+        {
+            IEnumerable<TEntity> entities = this.LoadTableEntities<TEntity>();
+            DbSet<TEntity> dbSetInstance = new DbSet<TEntity>(entities);
+            ReflectionHelper.ReplaceBackingField(this, dbSet.Name, dbSetInstance);
+        }
+
+        private void MapAllRelations()
+        {
+            foreach (var dbSetProperty in this.dbSetProperties)
+            {
+                Type dbSetType = dbSetProperty.Key;
+                object dbSet = dbSetProperty.Value.GetValue(this);
+
+                MethodInfo mapRelationsGeneric = typeof(DbContext)
+                    .GetMethod("MapRelations", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(dbSetType);
+
+
+                mapRelationsGeneric.Invoke(this, new object[] { dbSet });
+            }
+        }
+
+        private void MapRelations<TEntity>(DbSet<TEntity> dbSet)
+            where TEntity : class, new()
+        {
+            Type entityType = typeof(TEntity);
+
+            this.MapNavigationProperties(dbSet);
+
+            PropertyInfo[] collections = entityType
+                .GetProperties()
+                .Where(pi =>
+                    pi.PropertyType.IsGenericType &&
+                    pi.PropertyType.GetGenericTypeDefinition() == typeof(ICollection<>))
+                .ToArray();
+
+            foreach (PropertyInfo collection in collections)
+            {
+                Type collectionType = collection.PropertyType
+                    .GetGenericArguments()
+                    .First();
+
+                MethodInfo mapCollectionMethod = typeof(DbContext)
+                    .GetMethod("MapCollection", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(entityType, collectionType);
+
+                mapCollectionMethod.Invoke(this, new object[] { dbSet, collection });
+            }
+        }
+
+        private void MapCollection<TDbSet, TCollection>(DbSet<TDbSet> dbSet, PropertyInfo collectionProperty)
+            where TDbSet : class, new() 
+            where TCollection : class, new()
+        {
+            Type entityType = typeof(TDbSet);
+            Type collectionType = typeof(TCollection);
+
+            PropertyInfo[] primaryKeys = collectionType
+                .GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            PropertyInfo primaryKey = primaryKeys.First();
+
+            PropertyInfo foreignKey = entityType
+                .GetProperties()
+                .First(pi => pi.HasAttribute<KeyAttribute>());
+
+            bool isManyToMany = primaryKeys.Length >= 2;
+
+            if (isManyToMany)
+            {
+                primaryKey = collectionType
+                    .GetProperties()
+                    .First(pi => collectionType.GetProperty(pi.GetCustomAttribute<ForeignKeyAttribute>().Name)
+                    .PropertyType == entityType);
+                                  
+            }
+
+            DbSet<TCollection> navigationDbSet = (DbSet<TCollection>)this.dbSetProperties[collectionType].GetValue(this);
+
+            foreach (TDbSet entity in dbSet)
+            {
+                object primaryKeyValue = foreignKey.GetValue(entity);
+                TCollection[] navigationEntities = navigationDbSet
+                    .Where(ne => primaryKey.GetValue(ne).Equals(primaryKeyValue))
+                    .ToArray();
+                
+                ReflectionHelper.ReplaceBackingField(entity, collectionType.Name, navigationEntities);
+            }
+        }
+
+        private void MapNavigationProperties<TEntity>(DbSet<TEntity> dbSet) where TEntity : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<TEntity> LoadTableEntities<TEntity>()
+            where TEntity : class, new()
+        {
+            throw new NotImplementedException();
         }
 
         private string GetTableName(Type type)
@@ -127,15 +267,7 @@ namespace MiniORM
             throw new NotImplementedException();
         }
 
-        private void InitializeDbSets()
-        {
-            throw new NotImplementedException();
-        }
 
-        private void MapAllRelations()
-        {
-            throw new NotImplementedException();
-        }
     }
 
 }
