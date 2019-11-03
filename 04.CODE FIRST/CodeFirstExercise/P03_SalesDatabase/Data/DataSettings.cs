2907edbffdb99d7a1d85afc0a55426b6da9b811c
miraDask <mira.daskalov@gmail.com>
2019-11-03 15:17:46 +0000

    finished

---
+++ 04.CODE FIRST/CodeFirstExercise/P03_SalesDatabase/Data/DataSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    static class DataSettings
+    {
+       internal const string ConnectionString = @"Server=DESKTOP-2HCAHJD\SQLEXPRESS;Database=SalesDB;Integrated Security=True";
+    }
+}
---
+++ 04.CODE FIRST/CodeFirstExercise/P03_SalesDatabase/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace P03_SalesDatabase
+{
+    using Microsoft.EntityFrameworkCore;
+    using P03_SalesDatabase.Data;
+    
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new SalesContext();
+
+            db.Database.Migrate();
+
+
+        }
+    }
+}
