9b4299fe0a461135bf97c8e0d3a8618281b791e2
miraDask <mira.daskalov@gmail.com>
2019-11-07 21:56:35 +0000

    starting p03

---
+++ 05.ENTITY RELATIONS/EntityRelationsExercise/P03_FootballBetting/Data/DataSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    static class DataSettings
+    {
+        internal const string ConnectionString = @"Server=DESKTOP-2HCAHJD\SQLEXPRESS;Database=FootballBettingDb;Integrated Security=True";
+
+    }
+}
---
+++ 05.ENTITY RELATIONS/EntityRelationsExercise/P03_FootballBetting/Data/FootballBettingContext.cs
@@ -0,0 +1,29 @@
+﻿namespace P03_FootballBetting.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+        }
+
+        public FootballBettingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DataSettings.ConnectionString);
+            }
+        }
+
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
