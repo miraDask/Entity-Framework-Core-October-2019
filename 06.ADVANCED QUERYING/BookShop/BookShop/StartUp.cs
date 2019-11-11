e42c96cd6651ac1c60c6ca3343548bc29abd55e4
miraDask <mira.daskalov@gmail.com>
2019-11-11 12:46:59 +0000

    adding problem 1

--- 06.ADVANCED QUERYING/BookShop/BookShop/StartUp.cs
+++ 06.ADVANCED QUERYING/BookShop/BookShop/StartUp.cs
@@ -1,16 +1,38 @@
 ﻿namespace BookShop
 {
     using Data;
-    using Initializer;
+    using System;
+    using System.Text;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            using (var db = new BookShopContext())
-            {
-                DbInitializer.ResetDatabase(db);
-            }
+            var context = new BookShopContext();
+            
+            var command = Console.ReadLine();
+            var books = GetBooksByAgeRestriction(context, command);
+            Console.WriteLine(books);
+        }
+
+        //Problem 1:
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var sb = new StringBuilder();
+
+                var books = context
+                    .Books
+                    .Where(b => b.AgeRestriction.ToString().ToUpper() == command.ToUpper())
+                    .Select(b => new { 
+                        b.Title
+                    })
+                    .OrderBy(b => b.Title)
+                    .ToList();
+
+                books.ForEach(b => sb.AppendLine(b.Title));
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
