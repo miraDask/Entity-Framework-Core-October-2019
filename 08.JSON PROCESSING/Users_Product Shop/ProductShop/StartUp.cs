bcfe4af409714a78725351425fb3565f23d4f833
miraDask <mira.daskalov@gmail.com>
2019-11-15 11:57:39 +0000

    initial + adding problem 01

---
+++ 08.JSON PROCESSING/Users_Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.Linq;
+    using System.IO;
+
+    using Data;
+    using Models;
+    
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            var usersFromJson = File.ReadAllText(@"../../../Datasets/users.json");
+
+            Console.WriteLine(ImportUsers(context, usersFromJson));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            var validEntities = users
+                .Where(u => u.LastName != null && u.LastName.Length >= 3)
+                .ToList();
+
+            context.Users.AddRange(validEntities);
+            context.SaveChanges();
+
+            return $"Successfully imported {validEntities.Count}";
+        }
+    }
+}
