d5cd8c5335a7f9a5e00a36fdc61c0ec4f05d567b
miraDask <mira.daskalov@gmail.com>
2019-11-16 19:19:26 +0000

    adding problem 09

---
+++ 08.JSON PROCESSING/Users_Car Dealer/CarDealer/Data/CarDealerContext.cs
@@ -0,0 +1,77 @@
+﻿using CarDealer.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CarDealer.Data
+{
+    public class CarDealerContext : DbContext
+    {
+        public CarDealerContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public CarDealerContext()
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+        
+        public DbSet<Part> Parts { get; set; }
+        
+        public DbSet<PartCar> PartCars { get; set; }
+        
+        public DbSet<Sale> Sales { get; set; }
+        
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-2HCAHJD\SQLEXPRESS;Database=CarDealer;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PartCar>(e =>
+            {
+                e.HasKey(k => new { k.CarId, k.PartId });
+            });
+
+            modelBuilder.Entity<Car>(car =>
+            {
+                car.HasMany(c => c.PartCars)
+                    .WithOne(pc => pc.Car)
+                    .HasForeignKey(pc => pc.CarId);
+
+                car.HasMany(c => c.Sales)
+                    .WithOne(s => s.Car)
+                    .HasForeignKey(s => s.CarId);
+            });
+
+            modelBuilder.Entity<Part>(part =>
+            {
+                part.HasMany(p => p.PartCars)
+                    .WithOne(pc => pc.Part)
+                    .HasForeignKey(pc => pc.PartId);
+
+                part.HasOne(p => p.Supplier)
+                .WithMany(s => s.Parts)
+                .HasForeignKey(p => p.SupplierId);
+            });
+
+            modelBuilder.Entity<Customer>(cust =>
+            {
+                cust.HasMany(c => c.Sales)
+                    .WithOne(s => s.Customer)
+                    .HasForeignKey(s => s.CustomerId);
+
+            });
+        }
+    }
+}
---
+++ 08.JSON PROCESSING/Users_Car Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new CarDealerContext();
+
+            using (db)
+            {
+                //db.Database.EnsureCreated();
+                var suppliersFromJson = File.ReadAllText(@"./../../../Datasets/suppliers.json");
+                Console.WriteLine(ImportSuppliers(db, suppliersFromJson));
+
+            }
+        }
+        
+        //Problem 09:
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}."; ;
+        }
+    }
+}
