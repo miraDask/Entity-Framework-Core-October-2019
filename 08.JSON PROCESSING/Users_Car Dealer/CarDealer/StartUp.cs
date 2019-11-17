9a5f5f672365f4340c5e1a5309e9719352301ef7
miraDask <mira.daskalov@gmail.com>
2019-11-17 17:55:46 +0000

    adding problem 11

--- 08.JSON PROCESSING/Users_Car Dealer/CarDealer/StartUp.cs
+++ 08.JSON PROCESSING/Users_Car Dealer/CarDealer/StartUp.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
+using CarDealer.DTO;
 using CarDealer.Models;
 using Newtonsoft.Json;
 
@@ -17,18 +18,23 @@ namespace CarDealer
 
             using (db)
             {
+                //db.Database.EnsureDeleted();
                 //db.Database.EnsureCreated();
 
-                //Problem 09:
+                ////Problem 09:
                 //var suppliersFromJson = File.ReadAllText(@"./../../../Datasets/suppliers.json");
                 //Console.WriteLine(ImportSuppliers(db, suppliersFromJson));
 
-                var partsFromJson = File.ReadAllText(@"./../../../Datasets/parts.json");
-                Console.WriteLine(ImportParts(db, partsFromJson));
+                ////Problem 10:
+                //var partsFromJson = File.ReadAllText(@"./../../../Datasets/parts.json");
+                //Console.WriteLine(ImportParts(db, partsFromJson));
 
+                //Problem 11:
+                var carsFromJson = File.ReadAllText(@"./../../../Datasets/cars.json");
+                Console.WriteLine(ImportCars(db, carsFromJson));
             }
         }
-        
+
         //Problem 09:
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
         {
@@ -39,7 +45,7 @@ namespace CarDealer
             return $"Successfully imported {suppliers.Length}."; ;
         }
 
-        //Problem 09:
+        //Problem 10:
         public static string ImportParts(CarDealerContext context, string inputJson)
         {
             var parts = JsonConvert.DeserializeObject<Part[]>(inputJson);
@@ -52,11 +58,52 @@ namespace CarDealer
                 .Where(p => validSellersIds.Contains(p.SupplierId))
                 .ToList();
 
-
             context.Parts.AddRange(validEntities);
             context.SaveChanges();
 
             return $"Successfully imported {validEntities.Count}."; ;
+        }
+
+        //Problem 11:
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var carDtos = JsonConvert.DeserializeObject<CarDto[]>(inputJson);
+            
+            var partsIdInDb = context.Parts.Select(c => c.Id).ToList();
+            var cars = new List<Car>();
+            var partCars = new List<PartCar>();
+
+            foreach (var carDto in carDtos)
+            {
+                var car = new Car()
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TravelledDistance
+                };
+
+                foreach (var partId in carDto.PartsId.Distinct())
+                {
+                    if (!partsIdInDb.Contains(partId))
+                    {
+                        continue;
+                    }
+
+                    partCars.Add(new PartCar()
+                    {
+                        Car = car,
+                        PartId = partId
+                    });
+                }
+
+                cars.Add(car);
+            }
+
+            context.Cars.AddRange(cars);
+            context.PartCars.AddRange(partCars);
+            var r = context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}."; ;
         }
     }
 }
