6ca4a989aeb88722c911fdee4d2ff9ebe449398a
miraDask <mira.daskalov@gmail.com>
2019-11-25 13:51:53 +0000

    adding project 03.TemplatePattern

---
+++ 10.DESIGN PATTERNS/Exercise/Design_Patterns_Exercise/03.TemplatePattern/Bread.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.TemplatePattern
+{
+    using System;
+
+    public abstract class Bread
+    {
+        public abstract void MixIngredients();
+
+        public abstract void Bake();
+
+        public virtual void Slice()
+        {
+            Console.WriteLine($"Slicing the {this.GetType().Name} bread!");
+        }
+
+        // the tamlate method:
+        public void Make()
+        {
+            this.MixIngredients();
+            this.Bake();
+            this.Slice();
+        }
+    }
+}
---
+++ 10.DESIGN PATTERNS/Exercise/Design_Patterns_Exercise/03.TemplatePattern/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.TemplatePattern
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
