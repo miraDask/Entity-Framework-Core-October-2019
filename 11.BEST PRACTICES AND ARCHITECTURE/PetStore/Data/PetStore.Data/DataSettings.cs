4edfb497584023b8a0b621f35b628e307cad5998
miraDask <mira.daskalov@gmail.com>
2019-11-26 19:04:46 +0000

    adding Initial Migration and creating db

--- 11.BEST PRACTICES AND ARCHITECTURE/PetStore/Data/PetStore.Data/DataSettings.cs
+++ 11.BEST PRACTICES AND ARCHITECTURE/PetStore/Data/PetStore.Data/DataSettings.cs
@@ -2,6 +2,6 @@
 {
     internal static class DataSettings
     {
-        internal const string Connection = @"Server=DESKTOP-2HCAHJD\SQLEXPRESS;Database=SoftUni;Integrated Security=True;";
+        internal const string Connection = @"Server=DESKTOP-2HCAHJD\SQLEXPRESS;Database=PetStore;Integrated Security=True;";
     }
 }
--- 11.BEST PRACTICES AND ARCHITECTURE/PetStore/PetStore/StartUp.cs
+++ 11.BEST PRACTICES AND ARCHITECTURE/PetStore/PetStore/StartUp.cs
@@ -1,11 +1,19 @@
 ﻿namespace PetStore
 {
+    using Microsoft.EntityFrameworkCore;
+    using PetStore.Data;
     using System;
 
     public class StartUp
     {
         public static void Main()
         {
+            var db = new PetStoreDbContext();
+
+            using (db)
+            {
+                db.Database.Migrate();
+            }
         }
     }
 }
