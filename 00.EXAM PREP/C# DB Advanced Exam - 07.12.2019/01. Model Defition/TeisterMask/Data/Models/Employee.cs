30d9b0cccabdb48947fe8730929f232f7ce5742d
miraDask <mira.daskalov@gmail.com>
2019-12-09 11:58:28 +0000

    initial plus Model Definition

---
+++ 00.EXAM PREP/C# DB Advanced Exam - 07.12.2019/01. Model Defition/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace TeisterMask.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [RegularExpression(@"^([(A-Z0-9]*|[a-z0-9]*)$")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        public string Phone { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; } = new HashSet<EmployeeTask>();
+    }
+}
---
+++ 00.EXAM PREP/C# DB Advanced Exam - 07.12.2019/01. Model Defition/TeisterMask/Data/TeisterMaskContext.cs
@@ -0,0 +1,54 @@
+﻿namespace TeisterMask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using TeisterMask.Data.Models;
+
+    public class TeisterMaskContext : DbContext
+    {
+        public TeisterMaskContext() { }
+
+        public TeisterMaskContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<EmployeeTask> EmployeesTasks { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeTask>(entity =>
+            {
+                entity.HasKey(et => new { et.EmployeeId, et.TaskId });
+
+                entity.HasOne(et => et.Employee)
+                    .WithMany(e => e.EmployeesTasks)
+                    .HasForeignKey(et => et.EmployeeId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(et => et.Task)
+                    .WithMany(t => t.EmployeesTasks)
+                    .HasForeignKey(et => et.TaskId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Task>().HasOne(t => t.Project)
+                .WithMany(p => p.Tasks)
+                .HasForeignKey(t => t.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
